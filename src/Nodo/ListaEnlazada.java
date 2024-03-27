e7912ded5d45b7484cb57e8d12eedc26be69c936
Herland Mojica <mojicacardozoh@gmail.com>
2024-03-27 09:39:41 -0400

    cambios

--- src/Nodo/ListaEnlazada.java
+++ src/Nodo/ListaEnlazada.java
@@ -1,4 +1,69 @@
 package Nodo;
 
 public class ListaEnlazada {
+    public Nodo prim;
+    public int cantElem;
+    public Nodo ult;
+    public ListaEnlazada(){
+        prim = ult = null;
+        cantElem = 0;
+    }
+    public String toString(){
+        String s1 = "[";
+        Nodo p = prim;
+        while (p != null){
+            s1 = s1 + p.elem;
+            if (p.prox != null){
+                s1 = s1 + " ";
+            }
+            p = p.prox;
+        }
+        return s1 + "]";
+    }
+    //Metodos de lista
+    public boolean vacia(){
+        return ((prim == null) && (ult == null));
+    }
+    public void insertarPrim(int x){
+        if (vacia()){
+            prim = ult = new Nodo(x,null);
+        }else{
+            prim = new Nodo(x,prim);
+        }
+        cantElem++;
+    }
+    public void insertarUlt(int x){
+        if (vacia()){
+            prim = ult = new Nodo(x,null);
+        }else{
+            ult = ult.prox = new Nodo(x,null);
+        }
+    }
+    public void insertarIesimo(int x,int i){
+        int k = 0;
+        Nodo p = prim,ap = null;
+        while (p != null && k < i){
+            ap = p;
+            p = p.prox;
+            k++;
+        }
+        insertarNodo(x,ap,p);
+    }
+    public void insertarNodo(int x,Nodo ap,Nodo p){
+        if (ap == null){
+            insertarPrim(x);
+        }else{
+            if (p == null){
+                insertarUlt(x);
+            }else{
+                ap.prox = new Nodo(x,p);
+                cantElem++;
+            }
+        }
+    }
+    public boolean iguales(){
+        Nodo p = prim;
+        return true;
+    }
+
 }
--- src/Nodo/Nodo.java
+++ src/Nodo/Nodo.java
@@ -1,5 +1,11 @@
 package Nodo;
 
 public class Nodo {
-    
+    public int elem;
+    public Nodo prox;
+
+    public Nodo(int elem, Nodo prox) {
+        this.elem = elem;
+        this.prox = prox;
+    }
 }
